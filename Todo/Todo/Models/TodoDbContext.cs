83e357e5630ce11c1b413a18aa3a567f56a51f03
Hinthusiast <68813392+Hinthusiast@users.noreply.github.com>
2021-09-09 10:20:21 +0800

    added classes, In memory DBSeeder, DBProvider

---
+++ Todo/Todo/Models/TodoDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Todo.Models;
+public class TodoDbContext: DbContext
+{
+    public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options) { }
+
+    public DbSet<Todo> Todos { get; set; }
+}
---
+++ Todo/Todo/Models/TodoDbSeeder.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace Todo.Models;
+public class TodoDbSeeder
+{
+    public static void Seed(IServiceProvider serviceProvider)
+    {
+        using var context = new TodoDbContext(serviceProvider.GetRequiredService<DbContextOptions<TodoDbContext>>());
+        // Look for any todos.
+        if (context.Todos.Any())
+        {
+            //if we get here then the data already seeded
+            return;
+        }
+        context.Todos.AddRange(
+            new Todo
+            {
+                Id = 1,
+                TaskName = "Work on book chapter",
+                IsComplete = false
+            },
+            new Todo
+            {
+                Id = 2,
+                TaskName = "Create video content",
+                IsComplete = false
+            }
+            );
+        context.SaveChanges();
+
+    }
+}
