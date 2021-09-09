4a1ccc376d9ee388ce8b8be628a20595a838fdc8
Hinthusiast <68813392+Hinthusiast@users.noreply.github.com>
2021-09-09 13:19:09 +0800

    scaffolded a razor view page from the Controller Index method

---
+++ Todo/Todo/Controllers/TodoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Todo.Models;
+
+namespace Todo.Controllers;
+public class TodoController : Controller
+{
+    private readonly TodoDbContext _dbContext;
+    public TodoController(TodoDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    [HttpGet]
+    public IActionResult Index()
+    {
+        var todos = _dbContext.Todos.ToList();
+        return View(todos);
+    }
+}
